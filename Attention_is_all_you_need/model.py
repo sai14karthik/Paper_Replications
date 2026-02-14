5c074178d34e43e3056dae2936a5330925f9039d
Sai Karthik <nsaikarthik14@gmail.com>
2026-02-13 21:40:02 -0500

    add transformer

---
+++ Attention_is_all_you_need/model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__
+        self.d_model=d_model
+        self.seq_len=seq_len
+        self.dropout=nn.Dropout(dropout)
